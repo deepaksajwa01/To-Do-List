319abc1fd8ce72e6088907f26444b888a3c53e32
deepak sajwan <deepaksajwa01@gmail.com>
2017-09-26 11:33:10 +0530

    search filter added

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 
 var submitButton = document.getElementById("btn-submit");
 var list = document.getElementById("list");
-
+var searchValue = document.getElementById("searchValue");
 
 submitButton.addEventListener("click" , addItem);
 list.addEventListener("click" , removeItem);
-
-
-
+searchValue.addEventListener('keyup' ,filterList);
 
 
 function addItem(e){
   e.preventDefault();
   var value = document.getElementById("input-val").value;
-
-
   console.log(value);
 
   var li = document.createElement("li");
   var liText = document.createTextNode(value);
   var button = document.createElement("a");
-  button.appendChild(document.createTextNode("Delete"));
-  button.className="delete";
-
-  li.appendChild(button);
   li.appendChild(liText);
   list.appendChild(li);
+  button.appendChild(document.createTextNode("Delete"));
+  button.className="delete";
+  li.appendChild(button);
+  document.getElementById("input-val").value = " ";
+
 }
 
 function removeItem(e){
@@ -33,7 +30,22 @@ function removeItem(e){
     if(confirm("Are you sure want to delte"));
     var li = e.target.parentElement;
     list.removeChild(li);
-
   }
+}
 
+function filterList(e){
+  var text = e.target.value.toLowerCase();
+  var items = list.getElementsByTagName("li");
+  var itemsArray = Array.from(items);
+
+  itemsArray.forEach(function(item){
+    var content = item.firstChild.textContent;
+
+    if(content.toLocaleLowerCase().indexOf(text)!= -1){
+      item.style.display="block";
+    }
+    else{
+      item.style.display="none";
+    }
+  });
 }
